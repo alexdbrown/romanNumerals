0ba510575b5f9517be7e7b4295a40c5964dd1b62
christopher calascione & alexandra brown <student@epicodus.com>
2015-09-10 15:20:11 -0700

    build front end

--- js/scripts.js
+++ js/scripts.js
@@ -5,6 +5,11 @@ var romanNumerals = function(input) {
   var result = "";
   var inputTracker = input;
 
+  if (input < 1 || input > 3999) {
+    result = "Nope. Try again";
+    return result;
+  }
+
   for(var i = symbols.length - 1; i >= 0; i--) {
     var floor = Math.floor(inputTracker / symbols[i][1]);
     if (inputTracker != 0) {
@@ -14,3 +19,21 @@ var romanNumerals = function(input) {
   }
   return result;
 };
+
+$(document).ready(function() {
+
+  $("form#roman-numerals").submit(function(event) {
+    var number = parseInt($("input#number").val());
+    var result = romanNumerals(number);
+    if (result === "Nope. Try again") {
+      alert(result);
+    } else {
+      $(".number").text(number);
+
+      $(".results").text(result);
+
+      $("#result").show();
+      event.preventDefault();
+    }
+  });
+});
