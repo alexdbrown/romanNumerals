a51fedd316194170ceb49fa2327ab578f7dc8853
christopher calascione & alexandra brown <student@epicodus.com>
2015-09-10 14:51:13 -0700

    make it work

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,20 @@
 var romanNumerals = function(input) {
 
-  var symbols = [["I", 1], ["V", 5], ["X", 10], ["L", 50], ["C", 100], ["D", 500], ["M", 1000]];
+  var symbols = [["I", 1], ["IV", 4], ["V", 5], ["IX", 9], ["X", 10], ["XL", 40],
+  ["L", 50], ["XC", 90], ["C", 100], ["CD", 400], ["D", 500], ["DM", 900], ["M", 1000]];
   var result = "";
   var inputTracker = input;
+
   for(var i = symbols.length - 1; i >= 0; i--) {
-
-
     var floor = Math.floor(inputTracker / symbols[i][1]);
-    // var numberOfTimesDivisible = input / symbols[i][1];
-    result += symbols[i][0].repeat(floor);
-    inputTracker -= floor * symbols[i][0];
+    if (inputTracker != 0) {
+      result += symbols[i][0].repeat(floor);
+      inputTracker -= floor * symbols[i][1];
+    }
 
   }
+
+
+
   return result;
 };
--- specs/specs.js
+++ specs/specs.js
@@ -15,9 +15,21 @@ describe("romanNumerals", function() {
      expect(romanNumerals(6)).to.equal("VI");
    });
 
-  // it("takes 4 and prints IV", function() {
-  //   expect(romanNumerals(4)).to.equal("IV");
-  // });
+   it("takes 87 and prints LXXXVII", function() {
+     expect(romanNumerals(87)).to.equal("LXXXVII");
+   });
+
+  it("takes 4 and prints IV", function() {
+    expect(romanNumerals(4)).to.equal("IV");
+  });
+
+  it("takes 9 and prints IX", function() {
+    expect(romanNumerals(9)).to.equal("IX");
+  });
+
+  it("takes 19 a and prints XIX", function() {
+    expect(romanNumerals(19)).to.equal("XIX");
+  });
 
 
 });
