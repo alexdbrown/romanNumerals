5193803282c574b217df40a61a561281871b3f36
christopher calascione & alexandra brown <student@epicodus.com>
2015-09-10 12:16:40 -0700

    pass third test

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,16 @@
 var romanNumerals = function(input) {
-  var I = 1;
-  var V = 5;
-  var X = 10;
-  var L = 50;
-  var C = 100;
-  var D = 500;
-  var M = 1000;
 
+  var symbols = [["I", 1], ["V", 5], ["X", 10], ["L", 50], ["C", 100], ["D", 500], ["M", 1000]];
   var result = "";
 
-  for(var i = 0; i < input; i++) {
-    result = result + "I";
+  for(var i = symbols.length - 1; i >= 0; i--) {
+
+    if(input % symbols[i][1] === 0) {
+      var numberOfTimesDivisible = input /symbols[i][1];
+      result += symbols[i][0].repeat(numberOfTimesDivisible);
+      break;
+    }
+
   }
   return result;
-}
+};
--- specs/specs.js
+++ specs/specs.js
@@ -6,4 +6,18 @@ describe("romanNumerals", function() {
   it("takes 3 and prints III", function() {
     expect(romanNumerals(3)).to.equal("III");
   });
+
+  it("takes 1000 and prints M", function() {
+    expect(romanNumerals(1000)).to.equal("M");
+  });
+
+   it("takes 6 and prints VI", function() {
+     expect(romanNumerals(6)).to.equal("VI");
+   });
+
+  // it("takes 4 and prints IV", function() {
+  //   expect(romanNumerals(4)).to.equal("IV");
+  // });
+
+
 });
