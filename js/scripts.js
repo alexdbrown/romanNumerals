f173ee82feaac71c9eacd8ebf33cf424f30dc068
christopher calascione & alexandra brown <student@epicodus.com>
2015-09-10 12:31:43 -0700

    work on failling test

--- js/scripts.js
+++ js/scripts.js
@@ -2,14 +2,14 @@ var romanNumerals = function(input) {
 
   var symbols = [["I", 1], ["V", 5], ["X", 10], ["L", 50], ["C", 100], ["D", 500], ["M", 1000]];
   var result = "";
-
+  var inputTracker = input;
   for(var i = symbols.length - 1; i >= 0; i--) {
 
-    if(input % symbols[i][1] === 0) {
-      var numberOfTimesDivisible = input /symbols[i][1];
-      result += symbols[i][0].repeat(numberOfTimesDivisible);
-      break;
-    }
+
+    var floor = Math.floor(inputTracker / symbols[i][1]);
+    // var numberOfTimesDivisible = input / symbols[i][1];
+    result += symbols[i][0].repeat(floor);
+    inputTracker -= floor * symbols[i][0];
 
   }
   return result;
